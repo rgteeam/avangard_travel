6cbae6c4a384a60c0bae8a7b8fbbed3ee3c734c9
GKazhuro <georgefrimen@gmail.com>
2017-08-30 05:06:07 +0300

    back postgres again

--- avangard/settings.py
+++ avangard/settings.py
@@ -140,23 +140,23 @@ LOGIN_REDIRECT_URL = '/'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-     }
-}
-
 #DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME': 'avangardb',
-#        'USER': 'avangarduser',
-#        'PASSWORD': 'avangardpass',
-#        'HOST': 'localhost',
-#        'PORT': '',
-#    }
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
 #}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'avangardb',
+        'USER': 'avangarduser',
+        'PASSWORD': 'avangardpass',
+        'HOST': 'localhost',
+       'PORT': '',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
