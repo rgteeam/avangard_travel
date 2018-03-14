018f53990ca1522fbf2748f20cd552ee34c7c226
RuslanValeev <postroobotbox@gmail.com>
2018-03-14 03:27:14 +0300

    add company creation in settings page

--- avangard/setting/views.py
+++ avangard/setting/views.py
@@ -1,19 +1,33 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from avangard.setting.models import OrderSettings
+from avangard.museums.models import Company
+from django.http import Http404
 
 
 @login_required
 def settings_index(request):
     if request.method == 'GET':
-        current_value = OrderSettings.objects.latest('pk').prebuy_time
-        context = {'prebuy_time': current_value}
+        current_prebuy_time_value = OrderSettings.objects.latest('pk').prebuy_time
+        current_companies = Company.objects.all()
+        current_companies_str = ", ".join(i.name for i in current_companies)
+        context = {'prebuy_time': current_prebuy_time_value, 'current_companies': current_companies_str}
         return render(request, 'settings.html', context)
     elif request.method == 'POST':
         exist_item = OrderSettings.objects.latest('pk')
         exist_item.prebuy_time = int(request.POST.get("prebuy_time_input"))
         exist_item.save()
         current_value = OrderSettings.objects.latest('pk').prebuy_time
-        context = {'prebuy_time': current_value, 'type': 'updated'}
+
+        new_companies = str(request.POST.get("companies_input"))
+        for i in new_companies.split(', '):
+            try:
+                Company.objects.get(name=i)
+            except Company.DoesNotExist:
+                new_company = Company(name=i)
+                new_company.save()
+
+        current_companies = Company.objects.all()
+        current_companies_str = ", ".join(i.name for i in current_companies)
+        context = {'prebuy_time': current_value, 'current_companies': current_companies_str, 'type': 'updated'}
         return render(request, 'settings.html', context)
-        print("post request")
